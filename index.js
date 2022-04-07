af65843127d1ac787c882af1d91e403b6f4f9d32
Roger Zhang <1787009521@qq.com>
2022-04-07 22:27:40 +0800

    feat: 删除prev，next方法

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-(function() {
+(function () {
     let currentIndex = 0;
     let $slides = $('.slides')
     let $slidesWindow = $('.slidesWindow')
 
-    function playNextSlide() {
-        playSlideByIndex(currentIndex + 1)
-    }
-
-    function playPreviousSlide() {
-        playSlideByIndex(currentIndex - 1)
-    }
 
     function playSlideByIndex(index) {
+        if (index < 0) {
+            index = 4
+        } else if (index > 4) {
+            index = 0
+        }
         $slides.css({
             transform: `translateX(${-400 * (index)}px)`
         })
@@ -19,23 +17,23 @@
         return index
     }
 
-    buttonNext.onClick = function() {
-        playNextSlide()
+    buttonNext.onClick = function () {
+        playSlideByIndex(currentIndex + 1)
     }
 
-    buttonPrevous.onClick = function() {
-        playPreviousSlide()
+    buttonPrevous.onClick = function () {
+        playSlideByIndex(currentIndex - 1)
     }
 
     let timeId = setInterval(() => {
         playNextSlide()
     }, 3000)
 
-    $slidesWindow.on('mouseenter', function() {
+    $slidesWindow.on('mouseenter', function () {
         window.clearInterval(timeId)
     })
 
-    $slidesWindow.on('mouseleave', function() {
+    $slidesWindow.on('mouseleave', function () {
         timeId = setInterval(() => {
             playNextSlide()
         }, 3000)
