eb63d045acfcf782eb459ee9b37ee58f6c417865
Roger Zhang <1787009521@qq.com>
2022-04-12 22:41:12 +0800

    feat: 使用对象将所有属性放进对象，并且有个init方法

--- index.js
+++ index.js
@@ -1,63 +1,62 @@
 (function () {
-    let currentIndex = 0;
-    let $slides = $('.slides')
-    let $slidesWindow = $('.slidesWindow')
-    let timerId
 
+    let slide = {
+        currentIndex: 0,
+        $slides: $('.slides'),
+        $slidesWindow: $('.slidesWindow'),
+        $buttonNext: $('.buttonNext'),
+        $buttonPrevous: $('.buttonPrevous'),
+        timerId: null,
+        events: [
+            { el: slide.$buttonNext, event: 'click', fn: slide.playNext },
+            { el: slide.$buttonPrevous, event: 'click', fn: slide.playPrevious },
+            { el: slide.$slidesWindow, event: 'mouseenter', fn: slide.clearTimer },
+            { el: slide.$slidesWindow, event: 'mouseleave', fn: slide.resetTimer },
+        ],
+        playNext: () => {
+            slide.playSlideByIndex(slide.currentIndex + 1)
+        },
+        playPrevious: () => {
+            slide.playSlideByIndex(slide.currentIndex - 1)
+        },
 
-    function playSlideByIndex(index) {
-        index = fixIndex(index)
-        $slides.css({
-            transform: `translateX(${-400 * (index)}px)`
-        })
-        currentIndex = index
-        return index
+        clearTimer: () => {
+            window.clearInterval(slide.timerId)
+        },
+        resetTimer: () => {
+            slide.timerId = slide.autoPlay()
+        },
+        bindEvents: () => {
+            slide.events.forEach((eventObject) => {
+                $(eventObject.el).on(eventObject.event, eventObject.fn)
+            })
+        },
+        playSlideByIndex(index) {
+            index = slide.fixIndex(index)
+            slide.$slides.css({
+                transform: `translateX(${-400 * (index)}px)`
+            })
+            slide.currentIndex = index
+            return index
+        },
+        fixIndex(index) {
+            if (index < 0) {
+                index = 4
+            } else if (index > 4) {
+                index = 0
+            }
+            return index
+        },
+        autoPlay() {
+            return setInterval(() => {
+                playSlideByIndex(slide.currentIndex + 1)
+            }, 3000)
+        },
+        init() {
+            slide.timerId = slide.autoPlay()
+            slide.bindEvents()
+        },
     }
 
-    function fixIndex(index) {
-        if (index < 0) {
-            index = 4
-        } else if (index > 4) {
-            index = 0
-        }
-        return index
-    }
-
-    function autoPlay() {
-        return setInterval(() => {
-            playNextSlide(currentIndex + 1)
-        }, 3000)
-    }
-
-    let timerId = autoPlay()
-
-    let playNext = () => {
-        playSlideByIndex(currentIndex + 1)
-    }
-    let playPrevious = () => {
-        playSlideByIndex(currentIndex - 1)
-    }
-
-    let clearTimer = () => {
-        window.clearInterval(timeId)
-    }
-    let resetTimer = () => {
-        timerId = autoPlay()
-    }
-
-    function bindEvents() {
-        let events = [
-            { el: buttonNext, event: 'click', fn: playNext },
-            { el: buttonPrevous, event: 'click', fn: playPrevious },
-            { el: $slidesWindow, event: 'mouseenter', fn: clearTimer },
-            { el: $slidesWindow, event: 'mouseleave', fn: resetTimer },
-        ]
-    
-        events.forEach((eventObject) => {
-            $(eventObject.el).on(eventObject.event, eventObject.fn)
-        })
-    }
-    bindEvents()
-
-
+    slide.init() // slide.init.call(init)
 })
